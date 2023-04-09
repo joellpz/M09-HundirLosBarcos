735da4b5ad9283cb422d7721507dc73f9b96424c
joellpz <102882581+joellpz@users.noreply.github.com>
2023-04-09 18:18:30 +0200

    board showBoard

--- src/main/java/dam/m09/hundir/Board.java
+++ src/main/java/dam/m09/hundir/Board.java
@@ -5,12 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Board {
-    int [][] board = new int[8][8];
+    String [][] board = new String[8][8];
     /* El tablero se define por 4 posibles valores en cada posición.
-     0. Indica que esa casilla esta vacia.
-     1. Indica posicion ocupada por barco.
-     2. Indica posicion vacia ya marcada.
-     3. Indica barco tocado.
+     null. Indica que esa casilla esta vacia.
+     O. Indica posicion vacia ya marcada.
+     X. Indica barco tocado.
     */
 
     public Board() {
@@ -51,5 +50,17 @@ public class Board {
     ));
 
 
+    public void showBoardNoBoats(){
+        System.out.println("|---|---|---|---|---|---|---|---|");
+        for (int i = 0; i < board.length; i++) {
+
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == null) System.out.print("|   ");
+                else System.out.print("| "+board[i][j] +" ");
+            }
+            System.out.println("|");
+            System.out.println("|---|---|---|---|---|---|---|---|");
+        }
+    }
 
 }
