79849b0f0c02963c030902230cf4692776361d28
AngelCastroM <soporte@origamisoft.es>
2023-04-09 18:52:15 +0200

    initi

--- src/main/java/dam/m09/hundir/ThreadSevidorAdivina_Obj.java
+++ src/main/java/dam/m09/hundir/ThreadSevidorAdivina_Obj.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 public class ThreadSevidorAdivina_Obj implements Runnable {
     /* Thread que gestiona la comunicació de SrvTcPAdivina_Obj.java i un cllient ClientTcpAdivina_Obj.java */
 
-    private Socket clientSocket = null;
-    private InputStream in = null;
-    private OutputStream out = null;
+    private Socket clientSocket;
+    private InputStream in;
+    private OutputStream out;
     private Board board;
-    private boolean acabat;
+    private String jugada;
 
     public ThreadSevidorAdivina_Obj(Socket clientSocket, Board board) throws IOException {
         this.clientSocket = clientSocket;
@@ -24,37 +24,22 @@ public class ThreadSevidorAdivina_Obj implements Runnable {
 
     @Override
     public void run() {
-        Jugada j = null;
         try {
-            while(!acabat) {
-
+            while(true) {
                 //Enviem board al jugador
                 ObjectOutputStream oos = new ObjectOutputStream(out);
                 oos.writeObject(board);
                 oos.flush();
 
-                //Llegim la jugada
+                //Llegim la resposta del Client
                 ObjectInputStream ois = new ObjectInputStream(in);
                 try {
-                    j = (Jugada) ois.readObject();
+                    jugada = (String) ois.readObject();
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
-                System.out.println("jugada: " + j.Nom + "->" + j.num);
-                if(!board.map_jugadors.containsKey(j.Nom)) board.map_jugadors.put(j.Nom, 1);
-                else {
-                    //Si el judador ja esxiteix, actualitzem la quatitat de tirades
-                    int tirades = board.map_jugadors.get(j.Nom) + 1;
-                    board.map_jugadors.put(j.Nom, tirades);
-                }
 
-                //comprobar la jugada i actualitzar board amb el resultat de la jugada
-                board.resultat = ns.comprova(j.num);
-                if(board.resultat == 0) {
-                    acabat = true;
-                    System.out.println(j.Nom + " l'ha encertat");
-                    board.acabats++;
-                }
+
             }
         }catch(IOException e){
             System.out.println(e.getLocalizedMessage());
