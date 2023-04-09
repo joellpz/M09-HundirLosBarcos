789013280694b1a5146fed897ea849a2b5cb4748
joellpz <102882581+joellpz@users.noreply.github.com>
2023-04-09 17:19:27 +0200

    initi

--- src/main/java/dam/m09/hundir/Desk.java
+++ src/main/java/dam/m09/hundir/Desk.java
@@ -1,5 +1,13 @@
 package dam.m09.hundir;
 
 public class Desk {
+    int [][] desk = new int[8][8];
+    /* El tablero se define por 3 posibles valores en cada posición.
+     0. Indica que esa casilla esta vacia.
+     1. Indica posicion ocupada por barco.
+     2. Indica posicion vacia ya marcada.
+     3. Indica barco tocado.
+    */
+
 
 }
