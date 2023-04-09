d6325240c579d826a086fd3cc1ac62e05d8d44c7
joellpz <102882581+joellpz@users.noreply.github.com>
2023-04-09 17:27:51 +0200

    initi

---
+++ src/main/java/dam/m09/hundir/Boat.java
@@ -0,0 +1,14 @@
+package dam.m09.hundir;
+
+public class Boat {
+    int length;
+    String [] positions;
+    boolean [] touched;
+    public Boat(int length, String[] positions, boolean[] touched) {
+        this.length = length;
+        this.positions = positions;
+        this.touched = touched;
+    }
+
+
+}
